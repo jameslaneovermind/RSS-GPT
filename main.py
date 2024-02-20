5bc2225b53e6a59bee1a300dee725fc3ad329ece
jameslaneovermind <122231433+jameslaneovermind@users.noreply.github.com>
2024-02-20 14:42:50 +0000

    message update

--- main.py
+++ main.py
@@ -113,17 +113,11 @@ def truncate_entries(entries, max_entries):
         entries = entries[:max_entries]
     return entries
 
-def gpt_summary(query,model,language):
-    if language == "zh":
-        messages = [
-            {"role": "user", "content": query},
-            {"role": "assistant", "content": f"请用中文总结这篇文章，先提取出{keyword_length}个关键词，在同一行内输出，然后换行，用中文在{summary_length}字内写一个包含所有要点的总结，按顺序分要点输出，并按照以下格式输出'<br><br>总结:'，<br>是HTML的换行符，输出时必须保留2个，并且必须在'总结:'二字之前"}
-        ]
-    else:
-        messages = [
-            {"role": "user", "content": query},
-            {"role": "assistant", "content": f"You will be given an update from a companys status report. This report tells you the status of their platform. You need to first decide if you believe that the status update reflects a incident or outage. Then summarize this article in {language} language, first extract {keyword_length} keywords, output in the same line, then line break, write a summary containing all the points in {summary_length} words in {language}, output in order by points, and output in the following format '<br><br>Summary:' , <br> is the line break of HTML, 2 must be retained when output, and must be before the word 'Summary:'"}
-        ]
+def gpt_summary(query, model, language):
+    messages = [
+        {"role": "user", "content": query},
+        {"role": "assistant", "content": f"You will be given an update from a company's status report. This report tells you the status of their platform. You need to first decide if you believe that the status update reflects an incident or outage. Then summarize this article in {language} language, first extract {keyword_length} keywords, output them in the same line, then line break, write a summary containing all the points in {summary_length} words in {language}, output them in order by points, and output in the following format '<br><br>Summary:', <br> is the line break of HTML, 2 must be retained when output, and must be before the word 'Summary:'"}
+    ]
     client = OpenAI(
         api_key=OPENAI_API_KEY,
     )
